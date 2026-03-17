3e30a942e62c793f87b985e04842eb7736157179
jottarms-cyber <jottarms@gmail.com>
2026-03-17 13:57:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
-import Home from "./pages/Home";
-import Import from "./pages/Import";
-
 export default function App() {
-  const [page, setPage] = useState("home");
-
   return (
-    <>
-      {page === "home" && (
-        <Home goImport={() => setPage("import")} />
-      )}
-
-      {page === "import" && (
-        <Import goHome={() => setPage("home")} />
-      )}
-    </>
+    <div>
+      <h1>APP OK</h1>
+    </div>
   );
 }
