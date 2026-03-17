b5723467d23af84b0ebd97921714ea2f462b5edc
jottarms-cyber <jottarms@gmail.com>
2026-03-17 13:17:20 -0300

    Update BottomSheet.jsx

--- src/components/BottomSheet.jsx
+++ src/components/BottomSheet.jsx
@@ -2,15 +2,26 @@ export default function BottomSheet({ product, onClose }) {
   if (!product) return null;
 
   return (
-    <div className="sheet">
-      <h2 style={{ color: "#f15a00", marginTop: 0 }}>{product.codigo}</h2>
+    <div style={{
+      position: "fixed",
+      bottom: 0,
+      left: 0,
+      width: "100%",
+      height: "50%",
+      background: "white",
+      borderTopLeftRadius: "20px",
+      borderTopRightRadius: "20px",
+      padding: "20px",
+      boxShadow: "0 -2px 10px rgba(0,0,0,0.3)"
+    }}>
+      <h2 style={{ color: "#f15a00" }}>{product.codigo}</h2>
       <p>{product.descricao}</p>
 
       <button
         onClick={onClose}
         style={{
           marginTop: "20px",
-          padding: "10px 14px",
+          padding: "10px",
           border: "none",
           background: "#f15a00",
           color: "white",
