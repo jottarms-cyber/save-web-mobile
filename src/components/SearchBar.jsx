6fa0433fd4dac0f73ef87c5051b303260398cd11
jottarms-cyber <jottarms@gmail.com>
2026-03-17 01:39:04 -0300

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,13 +5,7 @@ export default function SearchBar({ query, setQuery }) {
       placeholder="Digite o código ou descrição"
       value={query}
       onChange={(e) => setQuery(e.target.value)}
-      style={{
-        width: "100%",
-        padding: "12px",
-        borderRadius: "10px",
-        border: "1px solid #ccc",
-        marginTop: "10px"
-      }}
+      className="search"
     />
   );
 }
