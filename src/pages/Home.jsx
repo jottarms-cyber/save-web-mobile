88ada98a0d71744cdd415423c864236ea4da2837
jottarms-cyber <jottarms@gmail.com>
2026-03-17 00:44:03 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,48 @@
-import { useState, useEffect } from "react";
-import SearchBar from "../components/SearchBar";
-import ProductTable from "../components/ProductTable";
-import BottomSheet from "../components/BottomSheet";
-import { filterProducts } from "../utils/filterProducts";
-import { getData } from "../services/storage";
+return (
+  <div>
 
-export default function Home({ goImport }) {
-  const [query, setQuery] = useState("");
-  const [products, setProducts] = useState([]);
-  const [selected, setSelected] = useState(null);
-
-  useEffect(() => {
-    getData().then(setProducts);
-  }, []);
-
-  const results = filterProducts(products, query);
-
-  return (
-    <div>
-      <h2>Save Web MOBILE</h2>
-
-      <SearchBar query={query} setQuery={setQuery} />
-
-      <ProductTable data={results} query={query} onSelect={setSelected} />
-
-      {selected && (
-        <BottomSheet product={selected} onClose={() => setSelected(null)} />
-      )}
-
-      <button onClick={goImport}>Importar Planilha</button>
+    {/* TOPO LARANJA */}
+    <div className="header">
+      <span style={{ fontSize: "22px" }}>SAVEweb MOBILE</span>
     </div>
-  );
-}
+
+    {/* TEXTO */}
+    <div style={{
+      background: "#f15a00",
+      color: "white",
+      padding: "8px",
+      fontWeight: "bold"
+    }}>
+      ▪ CONSULTA MERCADORIAS POR DESCRIÇÃO
+    </div>
+
+    {/* BUSCA */}
+    <SearchBar query={query} setQuery={setQuery} />
+
+    {/* TABELA */}
+    <ProductTable
+      data={results}
+      query={query}
+      onSelect={setSelected}
+    />
+
+    {/* DETALHE */}
+    {selected && (
+      <BottomSheet
+        product={selected}
+        onClose={() => setSelected(null)}
+      />
+    )}
+
+    {/* RODAPÉ */}
+    <div style={{
+      textAlign: "center",
+      marginTop: "20px",
+      color: "#0b3d2e",
+      fontWeight: "bold"
+    }}>
+      JOÃO PAULO - FILIAL 172 CASCAVEL
+    </div>
+
+  </div>
+);
