5f1004d2911c14552c37ce119cf9d0b97f9d9756
jottarms-cyber <jottarms@gmail.com>
2026-03-17 00:46:01 -0300

    Update ProductRow.jsx

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -2,11 +2,18 @@ import HighlightText from "./HighlightText";
 
 export default function ProductRow({ item, query, onClick }) {
   return (
-    <tr onClick={onClick} style={{ cursor: "pointer" }}>
-      <td>
+    <tr
+      onClick={onClick}
+      style={{
+        cursor: "pointer",
+        background: "#fff"
+      }}
+    >
+      <td style={{ padding: "10px", borderBottom: "1px solid #ddd" }}>
         <HighlightText text={item.codigo} query={query} />
       </td>
-      <td>
+
+      <td style={{ padding: "10px", borderBottom: "1px solid #ddd" }}>
         <HighlightText text={item.descricao} query={query} />
       </td>
     </tr>
